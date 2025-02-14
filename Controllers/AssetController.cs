a3515af5dff4aa3221f366c235eb0ff39321d8bc
Bui Duc Tri <buiductri179@gmail.com>
2025-02-14 20:25:43 +0700

    day tiep

---
+++ Controllers/AssetController.cs
@@ -0,0 +1,42 @@
+﻿using Azure_test_Battlegame.Data;
+using Azure_test_Battlegame.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure_test_Battlegame.Controllers;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetController : ControllerBase
+    {
+        private readonly BattleGameContext _context;
+
+        public AssetController(BattleGameContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("createasset")]
+        public async Task<IActionResult> CreateAsset([FromBody] Asset asset)
+        {
+            if (asset == null)
+            {
+                return BadRequest("Asset is null.");
+            }
+
+            asset.AssetId = Guid.NewGuid();
+            _context.Assets.Add(asset);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAssetById), new { id = asset.AssetId }, asset);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAssetById(Guid id)
+        {
+            var asset = await _context.Assets.FindAsync(id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+            return Ok(asset);
+        }
+    }
--- obj/Debug/net8.0/Azure_test_Battlegame.AssemblyInfo.cs
+++ obj/Debug/net8.0/Azure_test_Battlegame.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure_test_Battlegame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfabb7761b679856611ba9021b10323ba214ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure_test_Battlegame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azure_test_Battlegame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
